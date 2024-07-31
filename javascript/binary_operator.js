b093f36b98f8721911571537d6efb4b2eb2a7371
Satwik Bhagat <satwikbhagat@gmail.com>
2024-07-31 16:22:22 +0530

    ninth commit

---
+++ javascript/binary_operator.js
@@ -0,0 +1,25 @@
+// arithmetic operator
+// let a=10;
+// let b=2;
+// console.log(a+b) //10+2=12
+// console.log(a-b)//10-2=8
+// console.log(a*b)//10*2=20
+// console.log(a/b)//10/2=5
+// console.log(a%b)//10%2=0
+// console.log(a**b)//10**2=100
+
+//assignment operator
+
+// let user="amit sharma";
+// user=10
+// console.log(user)
+
+
+//compound assignment operator:- arithmetic operator +assignment operator
+
+// let a=10;
+// console.log(a/=2)
+// console.log(a%=2)
+
+// relational operator
+
---
+++ javascript/script.js
@@ -0,0 +1,49 @@
+// let a=1;
+// a=a++;
+// a=a++;
+// console.log(a);
+
+// let a=5;
+// a=a++; //5+1=6
+// a=a++;// 5+1=6
+// a=a++;//5+1=6
+// a=a++;//5+1=6
+// let b=a++; //a=5+1=6,b=5
+// b++;//b=6
+// console.log(a)//6
+// console.log(b)//6
+
+
+// let a=5;
+// a=a++; //5+1=6
+// a=a++;// 5+1=6
+// a=a++;//5+1=6
+// a=a++;//5+1=6
+// let b=a++*2; //a=5+1=6
+// b++;//b=6
+// console.log(a)//6
+// console.log(b)//6
+
+// question 7 post decrement operator
+// let a=5;
+// a=a--;
+// a=a--;
+// a=a--;
+// let b=++a + a-- -a++
+//     b--;
+// console.log(a)
+// console.log(b)
+
+
+// let a=1;
+// a=a++*2;
+// a=a++*2;
+// a=a++*2;
+// a=a++*2;
+
+// let b=(++a*3 - a++*2 + a--*2)/(a++*2)
+// b=(b++ + b--)*3/2
+// console.log(a)
+// console.log(b)
+
+
