ec01cec1b306b27f95ddf420611983fbbf879799
Satwik Bhagat <satwikbhagat@gmail.com>
2024-08-12 21:46:14 +0530

    17th commit

---
+++ java/oopsprog.java
@@ -0,0 +1,12 @@
+package interface_part;
+
+public class oopsprog
+{
+    public static void main(String[] args) 
+    {
+        int x=45;
+        System.out.println (x+100);
+        Integer y=45;
+        System.err.println (y+100);
+    }
+}
